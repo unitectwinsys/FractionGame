c6a9cfc914ffdc241f90c30de6bedb3b21fce789
Mercy Ventura <mercy.ventura@unitec.edu>
2013-05-03 23:25:36 -0600

    Adicion de Pruebas
    Denominador 0
    Inicializacion de Variables
    Suma de Fracciones

--- FractionGame/FractionGame/Fraccion.cs
+++ FractionGame/FractionGame/Fraccion.cs
@@ -8,6 +8,37 @@ namespace FractionGame
     public class Fraccion
     {
         public int Numerador { get; set; }
-        public int Denominador { get; set; }
+        public int Denominador
+        {
+            get;
+            set
+            {
+                if (value == 0) throw new DivideByZeroException("El Denominador no puede ser 0.");
+                Denominador = value;
+            }
+        }
+
+        //Constructores
+        public Fraccion()
+        {
+            this.Numerador = 1;
+            this.Denominador = 1;               
+        }
+        public Fraccion(int numerador, int denominador)
+        {
+            // TODO: Complete member initialization
+            this.Numerador = numerador;
+            this.Denominador = denominador;
+        }
+
+        //Metodos
+        public Fraccion sumar(Fraccion F1, Fraccion F2)
+        {
+            Fraccion Result = new Fraccion();
+            Result.Numerador = F1.Numerador + F2.Numerador;
+            Result.Denominador = F1.Denominador + F2.Denominador;
+            return Result;
+        }
+
     }
 }
--- FractionGame/TestProjectFraction/UnitTest1.cs
+++ FractionGame/TestProjectFraction/UnitTest1.cs
@@ -20,5 +20,40 @@ namespace TestProjectFraction
             bool b = (numerador == 1) && (denominador == 1);
             Assert.AreEqual(true, b);
         }
+
+        [TestMethod]
+        public void testSumarFracciones()
+        {
+            Fraccion F1 = new Fraccion(2, 3);
+            Fraccion F2 = new Fraccion(1, 3);
+
+            Fraccion FraccionExpected = new Fraccion(3, 3);
+            Fraccion resultado = new Fraccion();
+            resultado.sumar(F1, F2);
+
+            bool b = (resultado.Numerador == 3) && (resultado.Denominador == 3);
+            Assert.AreEqual(true, b);
+
+            //No existe esa clase dentro de VS, por ende, no interpreta el contenido y siempre daria False, compara las referencias
+            //Assert.AreEqual(resultado, FraccionExpected);
+        }
+
+        [TestMethod]
+        public void DenominadorNoCero()
+        {
+            //Evaluar que envie Excepcion
+            Fraccion F = new Fraccion();
+            try
+            {
+                F.Denominador = 0;
+            }
+            catch (DivideByZeroException D)
+            {
+                return;
+            }
+
+            Assert.Fail();
+        }
+
     }
 }
